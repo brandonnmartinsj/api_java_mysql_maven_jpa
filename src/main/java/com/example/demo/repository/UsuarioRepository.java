3bf9b945092bffb6eba34709752a8456eb446537
brandonnmartinsj <brandon@unesc.net>
2021-12-31 09:52:55 -0300

    first

---
+++ src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import com.example.demo.controller.model.UsuarioModel;
+import org.springframework.data.repository.CrudRepository;
+
+
+
+public interface UsuarioRepository extends CrudRepository <UsuarioModel, Integer>{
+
+
+
+
+
+}
